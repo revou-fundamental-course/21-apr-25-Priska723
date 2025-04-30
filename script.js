d4cb892ab1c96e0499ff8d03a2a1df2eb59d6ecc
Priska723 <priskaamalia723@gmail.com>
2025-04-30 20:26:21 +0700

    script.js

---
+++ script.js
@@ -0,0 +1,12 @@
+console.log('JavaScript is working!');
+
+function validateForm() {
+    const inputCelcius = document.getElementById('konversi-input');
+    console.log(inputCelcius.value);
+
+    if (inputCelcius.value === '') {
+        alert('Input tidak boleh kosong!');
+    } else {
+        /// Convert to fahrenheit
+    }
+}
